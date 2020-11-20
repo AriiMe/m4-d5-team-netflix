007f4125ab449145a59f73ecabda724303f81018
HolaSoyGonzalo <daved.pacco@gmail.com>
2020-11-20 11:24:54 +0100

    Update footer.jsx

---
+++ src/Components/footer.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <section class="link">
+      <div class="logo" style="padding-bottom: 30px; ">
+        <a href="#">
+          <img
+            src="img/facebook.svg"
+            width="30"
+            height="30"
+            style="margin-right: 10px"
+          />
+        </a>
+
+        <a href="#">
+          <img
+            src="img/instagram.svg"
+            width="30"
+            height="30"
+            style="margin-right: 10px"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="img/twitter.svg"
+            width="30"
+            height="30"
+            style="margin-right: 10px"
+          />
+        </a>
+        <a href="#">
+          <img
+            src="img/youtube.svg"
+            width="30"
+            height="30"
+            style="margin-right: 10px"
+          />
+        </a>
+      </div>
+      <div class="sub-links">
+        <ul>
+          <li>
+            <a href="#">Audio and Subtitles</a>
+          </li>
+          <li>
+            <a href="#">Audio Description</a>
+          </li>
+          <li>
+            <a href="#">Help Center</a>
+          </li>
+          <li>
+            <a href="#">Gift Cards</a>
+          </li>
+          <li>
+            <a href="#">Media Center</a>
+          </li>
+          <li>
+            <a href="#">Investor Relations</a>
+          </li>
+          <li>
+            <a href="#">Jobs</a>
+          </li>
+          <li>
+            <a href="#">Terms of Use</a>
+          </li>
+          <li>
+            <a href="#">Privacy</a>
+          </li>
+          <li>
+            <a href="#">Legal Notices</a>
+          </li>
+          <li>
+            <a href="#">Cookie Preferences</a>
+          </li>
+          <li>
+            <a href="#">Corporate Informations</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <button
+        type="button"
+        class="btn btn-outline"
+        style="background-color: #141414;border-color: #686868; color: #686868;"
+      >
+        Service Code
+      </button>
+    </section>
+  </div>
+);
+export default Footer;
