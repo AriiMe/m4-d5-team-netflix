fbeb7b941b08068887a6fef657ffebc33fd73eef
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-11-20 15:01:28 +0100

    search thingy done

--- src/Components/searchmf.jsx
+++ src/Components/searchmf.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-function Searchmf() {
+function Searchmf({handleInput, search}) {
     return (
         <section className="searchbox-wrap">
-            <input type="text" placeholder="Search for a movies..." className="searchbox"/>
+            <input 
+                type="text"
+                placeholder="Search for a movies..."
+                className="searchbox"
+                onChange={handleInput}
+                onKeyPress={search}
+            />
         </section>
     )
 }
 
-export default SearchmfS
+export default Searchmf
+
