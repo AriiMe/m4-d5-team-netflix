6584d26e86b76fa644b9fdc4ec0e8c36d64d860d
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2020-11-20 12:33:46 +0100

    Starting main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
-import Searchmf from "./Components/Searchmf"
+import Searchmf from "./Components/Searchmf";
+import Nav from "./Components/Nav";
+import Jumbo from "./Components/Jumbo";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Nav />
+      <Jumbo />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
