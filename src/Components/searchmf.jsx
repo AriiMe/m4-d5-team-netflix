6556d9a2d80a92298fa93cba71d6f6d49201e302
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-11-20 11:51:43 +0100

    brr

---
+++ src/Components/searchmf.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Searchmf() {
+    return (
+        <section className="searchbox-wrap">
+            <input type="text" placeholder="Search for a movies..." className="searchbox"/>
+        </section>
+    )
+}
+
+export default SearchmfS
