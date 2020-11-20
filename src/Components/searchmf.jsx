da861a627b3c53547abd3d6a5455527ba98378a0
HolaSoyGonzalo <daved.pacco@gmail.com>
2020-11-20 15:15:52 +0100

    Update searchmf.jsx

--- src/Components/searchmf.jsx
+++ src/Components/searchmf.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-function Searchmf() {
+function Searchmf({ handleInput, search }) {
   return (
     <section className="searchbox-wrap">
       <input
         type="text"
         placeholder="Search for a movies..."
         className="searchbox"
+        onChange={handleInput}
+        onKeyPress={search}
       />
     </section>
   );
