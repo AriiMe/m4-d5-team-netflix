627ee42e141891d76a78889874a9a352a5e2decb
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-11-26 10:38:54 +0100

    fixed jumbotron

--- src/Components/ShowDetail.jsx
+++ src/Components/ShowDetail.jsx
@@ -1,89 +1,124 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Container, Row, Col, Image, Jumbotron, Badge } from 'react-bootstrap';
-import Comments from './Comments';
-import Results from './Results'
-import RelatedMovies from './RelatedMovies'
+/** @format */
 
+import React from "react";
+import PropTypes from "prop-types";
+import { Container, Row, Col, Image, Jumbotron, Badge } from "react-bootstrap";
+import Comments from "./comments";
+import Results from "./Results";
+import RelatedMovies from "./RelatedMovies";
 
 class ShowDetail extends React.Component {
-    state = {
-        movie: {},
-        loaded: false,
+  state = {
+    movie: {},
+    loaded: false,
+  };
+  url = "http://www.omdbapi.com/?apikey=ff133ca5&";
+  load = () => {
+    let movie_id = "i=" + this.props.match.params.id;
+    this.fetchMovie(movie_id, 0);
+  };
+  constructor(props) {
+    super(props);
+    this.load();
+  }
+  related = () => {
+    const { movie, loaded } = this.state;
+    const { history } = this.props;
+    if (loaded === true) {
+      return (
+        <>
+          {movie.Type === "movie" ? (
+            <RelatedMovies history={history} Type="0" Id={movie.Title} />
+          ) : (
+            <RelatedMovies history={history} Type="1" Id={movie.imdbID} />
+          )}
+        </>
+      );
     }
-    url = "http://www.omdbapi.com/?apikey=ff133ca5&"
-    load = () => {
-        let movie_id = "i=" + this.props.match.params.id;
-        this.fetchMovie(movie_id, 0)
+  };
+  fetchMovie = async (id, type) => {
+    try {
+      let response = await fetch(this.url + id);
+
+      if (response.ok) {
+        let movie = await response.json();
+        return type === 0
+          ? this.setState({ movie, loaded: true })
+          : movie.Search;
+      }
+    } catch (e) {
+      console.log("error happened, that's life", e);
+      this.setState({ loading: false });
     }
-    constructor(props) {
-        super(props)
-        this.load();
-    }
-    related = () => {
-        const { movie, loaded } = this.state
-        const { history } = this.props
-        if (loaded === true) {
-            return <>{movie.Type === 'movie' ? <RelatedMovies history={history} Type='0' Id={movie.Title} /> : <RelatedMovies history={history} Type='1' Id={movie.imdbID} />}</>
-        }
-    }
-    fetchMovie = async (id, type) => {
-        try {
-            let response = await fetch(this.url + id);
+  };
 
-            if (response.ok) {
-                let movie = await response.json();
-                return type === 0 ? this.setState({ movie, loaded: true }) : movie.Search;
-            }
-        } catch (e) {
-            console.log("error happened, that's life", e)
-            this.setState({ loading: false })
-        }
-    }
+  render() {
+    const { movie } = this.state;
+    const { id } = this.props.match.params;
+    return (
+      <>
+        <Jumbotron
+          fluid
+          className="text-white"
+          style={{
+            width: "100%",
+            height: "90vh",
+            position: "relative",
+            backgroundImage: "url(" + movie.Poster + ")",
+            backgroundPosition: "center center",
+            backgroundSize: "cover",
+            paddingTop: "15vh",
+          }}
+        >
+          <Container
+            style={{ width: "100%", height: "100%", marginTop: "10vh" }}
+          >
+            <div>
+              <Image
+                src="https://image.flaticon.com/icons/png/512/870/870910.png"
+                height="40px"
+              />
+              <span className="text-uppercase font-weight-bold">
+                {movie.Type}
+              </span>
+            </div>
 
-    render() {
-        const { movie } = this.state;
-        const { id } = this.props.match.params;
-        return <>
-            <Jumbotron fluid className="text-white" style={{ width: "100%", height: "90vh", position: "relative", backgroundImage: 'url(' + movie.Poster + ')', backgroundPosition: "center center", backgroundSize: 'cover', paddingTop: '15vh' }}>
-                <Container style={{ width: "100%", height: "100%", marginTop: "10vh" }}>
-                    <div>
-                        <Image src="https://image.flaticon.com/icons/png/512/870/870910.png" height="40px" /><span className="text-uppercase font-weight-bold">{movie.Type}</span>
-                    </div>
-
-                    <Col lg={5} md={6}>
-                        <h1>{movie.Title}</h1>
-                        <Row className="justify-content-around mr-5">
-                            <small className=" font-weight-bold text-success">{movie.Metascore}% Match</small>
-                            <small className=" font-weight-bold text-muted">{movie.Type}</small>
-                            <small className=" font-weight-bold text-muted">{movie.Year}</small>
-                            <Badge variant="dark">{movie.Rated}</Badge>
-                            <Badge variant="dark">HD</Badge>
-                            <Badge variant="secondary">{movie.imdbRating}</Badge>
-                        </Row>
-                        <Row>
-
-                            <div className="plot mt-4">
-                                <p className=" font-weight-bolder text-white">{movie.Plot}</p>
-                            </div>
-                        </Row>
-                    </Col>
-                </Container>
-            </Jumbotron >
-            <Container>
-
-                <Col>
-                    <Comments id={id} />
-                </Col>
-                <Row id="related">
-                    <h2 className="text-white-50">Related</h2>
-                    {this.related()}
-                </Row>
-
-            </Container>
-
-        </>;
-    }
+            <Col lg={5} md={6}>
+              <h1>{movie.Title}</h1>
+              <Row className="justify-content-around mr-5">
+                <small className=" font-weight-bold text-success">
+                  {movie.Metascore}% Match
+                </small>
+                <small className=" font-weight-bold text-muted">
+                  {movie.Type}
+                </small>
+                <small className=" font-weight-bold text-muted">
+                  {movie.Year}
+                </small>
+                <Badge variant="dark">{movie.Rated}</Badge>
+                <Badge variant="dark">HD</Badge>
+                <Badge variant="secondary">{movie.imdbRating}</Badge>
+              </Row>
+              <Row>
+                <div className="plot mt-4">
+                  <p className=" font-weight-bolder text-white">{movie.Plot}</p>
+                </div>
+              </Row>
+            </Col>
+          </Container>
+        </Jumbotron>
+        <Container>
+          <Col>
+            <Comments id={id} />
+          </Col>
+          <Row id="related">
+            <h2 className="text-white-50">Related</h2>
+            {this.related()}
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
 ShowDetail.propTypes = { id: PropTypes.string };
--- src/Components/jumbo.jsx
+++ src/Components/jumbo.jsx
@@ -1,16 +1,49 @@
-import React from 'react';
-import {Jumbotron, Container, Image} from 'react-bootstrap';
+/** @format */
+
+import React from "react";
+import { Jumbotron, Container, Image } from "react-bootstrap";
+import "../App.css";
 
 class Jumbo extends React.Component {
-    render(){
-        return(
-
-                <Jumbotron fluid className='p-0 mainJumbo' style={{width: "100%", height:"100vh", position: "relative"}}>
-                            <embed type="image/svg+xml" src="https://tv.giphy.com/?username=hulu" style={{width: "80%", height: "100%", objectFit: "cover", float: "right"}}/>
-                            <div className='gradientOnJumbo' style={{position: "absolute", left: 0, height: "100%", width: "100%"}}></div>
-                </Jumbotron>
-        )
-    }
+  render() {
+    return (
+      <Jumbotron
+        fluid
+        className="p-0 jumbotron"
+        id="jumbotron"
+        style={{ width: "100%", height: "100vh", position: "relative" }}
+      >
+        <embed
+          type="image/svg+xml"
+          className="jumbotron"
+          src="https://tv.giphy.com/?username=hulu"
+          style={{
+            position: "absolute",
+            top: "0",
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            opacity: "0.5",
+          }}
+        />
+        <div
+          className="gradientOnJumbo"
+          style={{
+            content: "",
+            background: "rgb(20, 20, 20)",
+            background:
+              "linear-gradient(180deg, rgba(20, 20, 20, 0) 34%, rgba(20, 20, 20, 1) 100%)",
+            position: "absolute",
+            top: "0",
+            bottom: "0",
+            left: "0",
+            right: "0",
+            zIndex: 1,
+          }}
+        ></div>
+      </Jumbotron>
+    );
+  }
 }
 
 export default Jumbo;
